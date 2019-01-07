83aae89e54c6530132c82067390e2e21e42aad6d
3dwebart <3d.web.artist@gmail.com>
2019-01-07 09:25:56 +0900

    2018-01-04 Edit files : publish/theme/bkkorea/head.sub.php / publish/theme/bkkorea/skin/member/bk.member/style.css AND add file : publish/css/ie.css

--- publish/theme/bkkorea/head.sub.php
+++ publish/theme/bkkorea/head.sub.php
@@ -5,19 +5,19 @@ if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 $begin_time = get_microtime();
 
 if (!isset($g5['title'])) {
-    $g5['title'] = $config['cf_title'];
-    $g5_head_title = $g5['title'];
+	$g5['title'] = $config['cf_title'];
+	$g5_head_title = $g5['title'];
 }
 else {
-    $g5_head_title = $g5['title']; // 상태바에 표시될 제목
-    $g5_head_title .= " | ".$config['cf_title'];
+	$g5_head_title = $g5['title']; // 상태바에 표시될 제목
+	$g5_head_title .= " | ".$config['cf_title'];
 }
 
 // 현재 접속자
 // 게시판 제목에 ' 포함되면 오류 발생
 $g5['lo_location'] = addslashes($g5['title']);
 if (!$g5['lo_location'])
-    $g5['lo_location'] = addslashes(clean_xss_tags($_SERVER['REQUEST_URI']));
+	$g5['lo_location'] = addslashes(clean_xss_tags($_SERVER['REQUEST_URI']));
 $g5['lo_url'] = addslashes(clean_xss_tags($_SERVER['REQUEST_URI']));
 if (strstr($g5['lo_url'], '/'.G5_ADMIN_DIR.'/') || $is_admin == 'super') $g5['lo_url'] = '';
 
@@ -34,16 +34,16 @@ header("Pragma: no-cache"); // HTTP/1.0
 <meta charset="utf-8">
 <?php
 if (G5_IS_MOBILE) {
-    echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10,user-scalable=yes">'.PHP_EOL;
-    echo '<meta name="HandheldFriendly" content="true">'.PHP_EOL;
-    echo '<meta name="format-detection" content="telephone=no">'.PHP_EOL;
+	echo '<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=0,maximum-scale=10,user-scalable=yes">'.PHP_EOL;
+	echo '<meta name="HandheldFriendly" content="true">'.PHP_EOL;
+	echo '<meta name="format-detection" content="telephone=no">'.PHP_EOL;
 } else {
-    echo '<meta http-equiv="imagetoolbar" content="no">'.PHP_EOL;
-    echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">'.PHP_EOL;
+	echo '<meta http-equiv="imagetoolbar" content="no">'.PHP_EOL;
+	echo '<meta http-equiv="X-UA-Compatible" content="IE=edge">'.PHP_EOL;
 }
 
 if($config['cf_add_meta'])
-    echo $config['cf_add_meta'].PHP_EOL;
+	echo $config['cf_add_meta'].PHP_EOL;
 ?>
 <title><?php echo $g5_head_title; ?></title>
 <?php
@@ -76,11 +76,11 @@ var g5_cookie_domain = "<?php echo G5_COOKIE_DOMAIN ?>";
 <script src="<?php echo G5_JS_URL ?>/bootstrap.min.js"></script>
 <?php
 if (defined('_SHOP_')) {
-    if(!G5_IS_MOBILE) {
+	if(!G5_IS_MOBILE) {
 ?>
 <script src="<?php echo G5_JS_URL ?>/jquery.shop.menu.js?ver=<?php echo G5_JS_VER; ?>"></script>
 <?php
-    }
+	}
 } else {
 ?>
 <script src="<?php echo G5_JS_URL ?>/jquery.menu.js?ver=<?php echo G5_JS_VER; ?>"></script>
@@ -101,26 +101,33 @@ if (defined('_SHOP_')) {
 <link rel="stylesheet" href="<?php echo G5_CSS_URL ?>/custom.css">
 <?php
 if(defined('_INDEX_')) { // index에서만 실행
-    echo '<link rel="stylesheet" href="'.G5_CSS_URL.'/index/index.css">';
+	echo '<link rel="stylesheet" href="'.G5_CSS_URL.'/index/index.css">';
 }
-?>
-<?php
+
+/* BIGIN :: IE private CSS */
+$userAgent = $_SERVER["HTTP_USER_AGENT"];
+if ( preg_match("/MSIE*/", $userAgent) ) {
+	echo '<link rel="stylesheet" href="'.G5_CSS_URL.'/ie.css">';
+} else if( preg_match("/^Trident/", $userAgent &&  preg_match("/^rv:11.0/", $userAgent &&  reg_match("/^Gecko/", $userAgent))) ) {
+	echo '<link rel="stylesheet" href="'.G5_CSS_URL.'/ie.css">';
+}
+/* END :: IE private CSS */
 if(G5_IS_MOBILE) {
-    echo '<script src="'.G5_JS_URL.'/modernizr.custom.70111.js"></script>'.PHP_EOL; // overflow scroll 감지
+	echo '<script src="'.G5_JS_URL.'/modernizr.custom.70111.js"></script>'.PHP_EOL; // overflow scroll 감지
 }
 if(!defined('G5_IS_ADMIN'))
-    echo $config['cf_add_script'];
+	echo $config['cf_add_script'];
 ?>
 </head>
 <body<?php echo isset($g5['body_script']) ? $g5['body_script'] : ''; ?>>
 <?php
 if ($is_member) { // 회원이라면 로그인 중이라는 메세지를 출력해준다.
-    $sr_admin_msg = '';
-    if ($is_admin == 'super') $sr_admin_msg = "최고관리자 ";
-    else if ($is_admin == 'group') $sr_admin_msg = "그룹관리자 ";
-    else if ($is_admin == 'board') $sr_admin_msg = "게시판관리자 ";
+	$sr_admin_msg = '';
+	if ($is_admin == 'super') $sr_admin_msg = "최고관리자 ";
+	else if ($is_admin == 'group') $sr_admin_msg = "그룹관리자 ";
+	else if ($is_admin == 'board') $sr_admin_msg = "게시판관리자 ";
 
-    echo '<div id="hd_login_msg">'.$sr_admin_msg.get_text($member['mb_nick']).'님 로그인 중 ';
-    echo '<a href="'.G5_BBS_URL.'/logout.php">로그아웃</a></div>';
+	echo '<div id="hd_login_msg">'.$sr_admin_msg.get_text($member['mb_nick']).'님 로그인 중 ';
+	echo '<a href="'.G5_BBS_URL.'/logout.php">로그아웃</a></div>';
 }
 ?>
