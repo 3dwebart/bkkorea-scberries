372ee7719c310f040f9355b8f56f3fbb94ae9604
3dwebart <3d.web.artist@gmail.com>
2019-01-22 09:50:53 +0900

    2019-01-22 Edit files : publish/css/custom_mobile.css, publish/theme/bkkorea/mobile/shop/js/mobile-custom.js / Add files : publish/css/swiper.min.css, publish/img/swipe_slide_arrow.png, publish/js/swiper.min.js, publish/js/swiper.min.js.map

--- publish/theme/bkkorea/mobile/shop/js/mobile-custom.js
+++ publish/theme/bkkorea/mobile/shop/js/mobile-custom.js
@@ -7,27 +7,34 @@ jQuery(document).ready(function($) {
 	var menu_bg = '<div class="cate_bg"></div>';
 	
 	$("#btn_hdcate").on("click", function() {
-        //$("#category").show();
-        $("#category").after(menu_bg);
-        setTimeout(function() {
-	        $('.cate_bg').addClass('on');
-        }, 100);
-        $("#category").addClass('on');
-        //$('.cate_bg').animate({}, 300);
-    });
+		//$("#category").show();
+		$("#category").after(menu_bg);
+		setTimeout(function() {
+			$('.cate_bg').addClass('on');
+		}, 100);
+		$("#category").addClass('on');
+		//$('.cate_bg').animate({}, 300);
+	});
 
-    $(".menu_close").on("click", function() {
-	    $("#category").removeClass('on');
-        $('.cate_bg').removeClass('on');
-        setTimeout(function() {
-	        $('.cate_bg').remove();
-        }, 500);
-    });
-     $(".cate-menu-bg").on("click", function() {
-        $("#category").removeClass('on');
-        $('.cate_bg').removeClass('on');
-        setTimeout(function() {
-	        $('.cate_bg').remove();
-        }, 500);
-    });
+	$(".menu_close").on("click", function() {
+		$("#category").removeClass('on');
+		$('.cate_bg').removeClass('on');
+		setTimeout(function() {
+			$('.cate_bg').remove();
+		}, 500);
+	});
+	 $(".cate-menu-bg").on("click", function() {
+		$("#category").removeClass('on');
+		$('.cate_bg').removeClass('on');
+		setTimeout(function() {
+			$('.cate_bg').remove();
+		}, 500);
+	});
+	jQuery('#sit_inf_open').hide();
+	jQuery('#sit_inf #sit_inf_open td').each(function() {
+		var thisText = jQuery(this).text();
+		if(thisText != '상품페이지 참고') {
+			jQuery(this).closest('table').show();
+		}
+	});
 });
